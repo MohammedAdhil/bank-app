f41f2361aa53e8f2553a67ec62997b31c2d99b8d
Home <18gcs09@meaec.edu.in>
2022-12-02 12:17:02 +0530

    First commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  aim="your perfect banking partner"
+  data="Enter your account number"
+  acno=''
+  psw=''
+  userDetails:any={
+    1000:{acno:1000,username:"anu",password:123,balance:0},
+    1001:{acno:1001,username:"amal",password:123,balance:0},
+    1002:{acno:1002,username:"arun",password:123,balance:0},
+    1003:{acno:1003,username:"mega",password:123,balance:0},
+  }
+  login(){
+    alert('login clicked')
+  }
+  acnoChange(event:any){
+
+    this.acno=event.target.value
+  }
+  pswChange(event:any){
+
+    this.psw=event.target.value
+    console.log(this.psw);
+    
+  }
+}
+
